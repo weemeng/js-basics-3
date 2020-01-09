84e2ea2a0eda9ec9e77d231a712f4060082f8045
weemeng <weemeng@users.noreply.github.com>
2020-01-09 15:23:57 +0800

    JS3

--- exercise-4.js
+++ exercise-4.js
@@ -1,31 +1,36 @@
+const assert = require("assert");
+
 // 1. Create an array `names` with at least 3 elements: `'david'`, `'gordon'`, and
 //    `'sahil'` and log it to the console
-
+const names = [`david`, `gordon`,`sahil`];
+console.log(names);
+// assert.equal(names, [`david`, `gordon`,`sahil`]);
 
 // 2. Log the value of the third element in `names` to the console
-
+assert.strictEqual(names[2], `sahil`);
 
 // 3. Log the value of the `names` array's `length` property to the console
-
+assert.strictEqual(names.length, 3);
 
 // 4. Set the value of the `length` property to `1`
-
+names.length = 1;
 
 // 6. Once again, log the value of the `names` array's `length` property to the
 //    console. Did it change?
-
+console.log(names.length)
 
 // 7. Log the entire `names` array to the console again
-
+console.log(names)
 
 // 8. Add a few more elements to the array using `push`
-
+names.push(`gordon`);
+names.push(`sahil`);
 
 // 9. Log the last element of the array to the console
-
+assert.strictEqual(names[2],`sahil`);
 
 // 10. Log the second last element of the array to the console
-
+console.log(names[names.length-2]);
 
 // 11. Change the `textContent` property of the _second_ `p` tag on the page to
 //    `"JavaScript was here!"`
@@ -34,6 +39,16 @@
 // 12. Convert the `names` array to a string, with each element separated by a
 //    '<br>'. Store it in a new variable called `namesString` e.g. if `names` is
 //    `['harry', 'ron', 'dean']` then `namesString` will be `"harry<br>ron<br>dean"`
+// let namesString = names[0];
+// for(i=1;i<names.length;i++) {
+//     namesString += '<br>' + names[i]
+
+// }
+// console.log(namesString)
+
+namesString = names.join('<br>');
+console.log(namesString);
+
 
 
 // 13. Change the `innerHTML` property of the _first_ `p` tag on the page to
--- exercise-5.js
+++ exercise-5.js
@@ -1,7 +1,13 @@
 // 1. Create a new array `shoppingList` with a few string elements
+shoppingList = [`orange`,`papaya`,`KFC`];
 
+// 2a. Using `forLoop`, log every element of the shoppingList` array to the console
+for (i=0;i<shoppingList.length;i++) {
+    console.log(shoppingList[i]);
+}
 
-// 2. Using `forEach`, log every element of the `shoppingList` array to the console
+// 2b. Using `forEach`, log every element of the `shoppingList` array to the console
+shoppingList.forEach((element) => {console.log(element)});
 
 
 // 3. For each element in the `shoppingList` array, create an `<li>` node and
@@ -10,8 +16,16 @@
 
 
 // 4. Create a new array `numbers` with a few numeric elements
-
+numbers = [12,34,55,67,89];
 
 // 5. Using `map` on the `numbers` array, create a new array called
 //    `squareNumbers` where each element is the square of the corresponding
 //    element in the `numbers` array
+let squareNumbers = [];
+for(i=0;i<numbers.length;i++) {
+    squareNumbers.push(Math.pow(numbers[i],2));
+}
+console.log(squareNumbers)
+
+squareNumbers = numbers.map((val) => (Math.pow(val,2)));
+console.log(squareNumbers)
